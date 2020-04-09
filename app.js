5e87be2bd0b679895a99d775975665b8ca7eaaba
Edward Kim <edk217@gmail.com>
2020-04-08 21:31:39 -0400

    drag partially working

--- app.js
+++ app.js
@@ -26,16 +26,41 @@ d3.csv("bookings.csv").then((data) => {
     const mostRecentDate = new Date(d3.max(data, (d) => {
         return d.date
     }))
+    
     let forecastEndDate = new Date(mostRecentDate)
     forecastEndDate = forecastEndDate.setFullYear(forecastEndDate.getFullYear() + 1)
+    
 
     const firstDate = d3.min(data, (d) => {
         return d.date
     })
 
+    const linearRegression = d3.regressionLinear()
+        .x(d => d.date)
+        .y(d => d.value)
+
+
+
+    const slope = linearRegression(data).a
+    const yIntercept = linearRegression(data).b
+    let forecastData = []
+
+    let futureMonth = new Date(mostRecentDate)
+    while (futureMonth < forecastEndDate) {
+        if (futureMonth.getMonth() == 11) {
+            futureMonth = new Date(futureMonth.getFullYear() + 1, 1, 0);
+        } else {
+            futureMonth = new Date(futureMonth.getFullYear(), futureMonth.getMonth() + 2, 0);
+        }
+        let futureValue = slope * futureMonth + yIntercept
+        forecastData.push({
+            date: futureMonth,
+            value: futureValue
+        })
+    }
 
     xScale.domain([firstDate, forecastEndDate])
-    yScale.domain([0, d3.max(data, function (d) {
+    yScale.domain([0, d3.max(forecastData, function (d) {
         return d.value
     })])
 
@@ -51,6 +76,12 @@ d3.csv("bookings.csv").then((data) => {
         .attr("class", "axis")
         .attr("transform", `translate(0,${height - margin.bottom})`)
         .call(xaxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.5em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-45)");
+        
 
 
     svg.append("g")
@@ -66,102 +97,116 @@ d3.csv("bookings.csv").then((data) => {
 
 
 
+    // Add historical data as a line plot
     const line = d3.line()
-        .x(function (d, i) { return xScale(d.date); }) // set the x values for the line generator
-        .y(function (d) {  return yScale(d.value); }) // set the y values for the line generator 
-        .curve(d3.curveMonotoneX) // apply smoothing to the line
-
-
-    // const line = d3.line()
-    //     .x(function (d) { return xScale(d.date) })
-    //     .y(function (d) { return yScale(d.value) })
+        .x(function (d, i) { return xScale(d.date) })
+        .y(function (d) { return yScale(d.value) })
+        .curve(d3.curveMonotoneX)
     
     svg.append("path")
-        .datum(data) // 10. Binds data to the line 
-        .attr("class", "line") // Assign a class for styling 
-        .attr("d", line); // 11. Calls the line generator 
+        .datum(data) 
+        .attr("class", "line") 
+        .attr("d", line); 
 
-    // 12. Appends a circle for each datapoint 
+    // Attach a circle to each datapoint 
     svg.selectAll(".dot")
         .data(data)
         .enter().append("circle") // Uses the enter().append() method
         .attr("class", "dot") // Assign a class for styling
         .attr("cx", function (d) { return xScale(d.date) })
         .attr("cy", function (d) { return yScale(d.value) })
-        .attr("r", 5)
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+        .attr("r", 2)
 
-    const linearRegression = d3.regressionLinear()
-        .x(d => d.date)
-        .y(d => d.value)
+    // Add vertical line separating past and present    
+    svg.append("line")
+        .attr("class", "present-day")
+        .attr("x1", xScale(Date.now()))
+        .attr("x2", xScale(Date.now()))
+        .attr("y1", yScale(0))
+        .attr("y2", yScale(d3.max(forecastData, function (d) {
+            return d.value
+        })))
 
 
-    
-    const slope = linearRegression(data).a
-    const yIntercept = linearRegression(data).b
-    let forecastData = []
+    const forecastLine = d3.line()
+        .x(function(d, i) { return xScale(d.date) })
+        .y(function(d) { return yScale(d.value) })
+        .curve(d3.curveMonotoneX)
 
-    let futureMonth = new Date(mostRecentDate)
-    while (futureMonth < forecastEndDate) {
-        if (futureMonth.getMonth() == 11) {
-            futureMonth = new Date(futureMonth.getFullYear() + 1, 1, 0);
-        } else {
-            futureMonth = new Date(futureMonth.getFullYear(), futureMonth.getMonth() + 2, 0);
-        }
-        console.log(futureMonth)
-        let futureValue = slope * futureMonth + yIntercept
-        forecastData.push({
-            date: futureMonth,
-            value: futureValue
-        })
-    }
 
+    svg.append("path")
+        .datum(forecastData)
+        .attr("class", "forecast-line")
+        .attr("id", "forecast1")
+        .attr("d", forecastLine);
+
+    // Regression line (suggested forecast #1)
+    const lastForecast = forecastData[forecastData.length - 1]
+    // svg.append("line")
+    //     .attr("class", "forecast")
+    //     .attr("x1", xScale(forecastData[0].date))
+    //     .attr("x2", xScale(lastForecast.date))
+    //     .attr("y1", yScale(forecastData[0].value))
+    //     .attr("y2", yScale(lastForecast.value))
 
     // Add dots for forecasted values
     svg.selectAll(".forecast-dot")
         .data(forecastData)
-        .enter().append("circle") // Uses the enter().append() method
-        .attr("class", "forecast-dot") // Assign a class for styling
+        .enter().append("circle") 
+        .attr("class", "forecast-dot")
         .attr("cx", function (d) { return xScale(d.date) })
         .attr("cy", function (d) { return yScale(d.value) })
-        .attr("r", 5)
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+        .attr("r", 2)
 
-    // console.log(forecastData)
-    // svg.append("line")
-    //     .attr("class", "regression")
-    //     .datum(linearRegression(data))
-    //     .attr("x1", d => xScale(d[0][0]))
-    //     .attr("x2", d => xScale(forecastEndDate))
-    //     .attr("y1", d => yScale(d[0][1]))
-    //     .attr("y2", d => yScale(forecastEndDate * slope + yIntercept));
+    // Line connecting most recent data point to forecast
+    const lastDataPoint = data[data.length - 1]
+    svg.append("line")
+        .attr("class", "forecast-line")
+        .attr("x1", xScale(lastDataPoint.date))
+        .attr("x2", xScale(forecastData[0].date))
+        .attr("y1", yScale(lastDataPoint.value))
+        .attr("y2", yScale(forecastData[0].value))
+
+    // Line continuing most recent value (suggested forecast #2)
+    svg.append("line")
+        .attr("class", "forecast-line")
+        .attr("x1", xScale(lastDataPoint.date))
+        .attr("x2", xScale(lastForecast.date))
+        .attr("y1", yScale(lastDataPoint.value))
+        .attr("y2", yScale(lastDataPoint.value))
+
+    // let forecast2Data = {}
+    for (var f of forecastData) {
+        svg.append("circle") 
+            .attr("class", "forecast-dot") 
+            .attr("cx", xScale(f.date))
+            .attr("cy", yScale(lastDataPoint.value))
+            .attr("r", 2)
+        // forecast2Data.push({
+        //     date: f.date,
+        //     value: lastDataPoint.value
+        // })
+    }
+
+    // Add dragging effects
+    d3.selectAll(".forecast-dot").call(d3.drag()
+        .on("start", dragStart)
+        .on("drag", dragging)
+        .on("end", dragEnd))
+    function dragStart() {
+        d3.select(this).raise().attr("r", 4)
+        console.log(forecastData)
+    }
+    function dragging(d) {
+        d3.select(this).attr("cy", d.y = d3.event.y)
+        d.value = yScale.invert(d.y)
+    }
+    function dragEnd(d) {
+        d3.select(this).attr("r", 2)
+        console.log(forecastData)
+        d3.select("#forecast1").attr("d", forecastLine)
+        //TODO recalculate forecast line
+    }
 
 })
-
-
-
-function handleMouseOver(d, i) {  // Add interactivity
-    // Use D3 to select element, change color and size
-    d3.select(this).attr("class", "focus");
-
-    // // Specify where to put label of text
-    svg.append("text").attr({
-        x: function () { return xScale(d.date) - 30; },
-        y: function () { return yScale(d.value) - 15; }
-    }).text(function () {
-            return [d.date, d.value];  // Value of the text
-        });
-}
-
-function handleMouseOut(d, i) {
-    // Use D3 to select element, change color back to normal
-    d3.select(this).attr("class", "dot");
-
-    // Select text by id and then remove
-    d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
-}
-
-
 
